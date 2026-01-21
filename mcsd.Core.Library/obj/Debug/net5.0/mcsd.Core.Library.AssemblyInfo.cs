e5d529d4a4f449dcc4e3f28b1a25473df5f2fc52
aperezNWO <alejandro.perez.acosta@hotmail.com>
2026-01-20 21:39:52 -0500

    working folder updte

--- mcsd.Core.Library/obj/Debug/net5.0/mcsd.Core.Library.AssemblyInfo.cs
+++ mcsd.Core.Library/obj/Debug/net5.0/mcsd.Core.Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mcsd.Core.Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bbba3124db2542f5520d1ebb1d97b62791b9d5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af040d2f43a50c3467c3d11ea3faae190210f84")]
 [assembly: System.Reflection.AssemblyProductAttribute("mcsd.Core.Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mcsd.Core.Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
